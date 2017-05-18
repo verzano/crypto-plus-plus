8b7efc6a19af9a55a8b23cf5ccdad1a1744989f0
verzano <mitchell.v.berg@gmail.com>
2017-05-18 13:23:36 +0200

    added constructor and more skelly

--- include/trifid.h
+++ include/trifid.h
@@ -3,18 +3,23 @@
 
 #include "polybiussquare.h"
 
+#define KEYS {"1", "2", "3"}
+
+// TODO make a PolybiusCube that is conceptually the three squares as one
 namespace cryptoplusplus {
-class trifid : public Cipher<string, string> {
+class Trifid : public Cipher<string, string> {
  private:
   PolybiusSquare square1;
   PolybiusSquare square2;
   PolybiusSquare square3;
+  unsigned long period;
 
  public:
+  Trifid(unsigned long period, vector<string> alphabet1, vector<string> alphabet2, vector<string> alphabet3)
+      : period(period), square1(KEYS, alphabet1), square2(KEYS, alphabet2), square3(KEYS, alphabet3) {};
   string encrypt(string plainText);
   string decrypt(string cipherText);
 };
 }
-
 
 #endif
--- src/trifid.cpp
+++ src/trifid.cpp
@@ -1,5 +1,11 @@
 #include "trifid.h"
 
 namespace cryptoplusplus {
+string Trifid::encrypt(string plainText) {
+  return plainText;
+}
 
+string Trifid::decrypt(string cipherText) {
+  return cipherText;
+}
 }
